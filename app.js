16ef317b4fc19797787edd083fc79d007953cb7b
cdROma <truemrv@gmail.com>
2020-04-06 13:01:45 +0300

    🙋code added

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const http = require('http');
+const serverPort = 3460;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
+
+app.get('/:id', (req, res) => {
+    send(req)
+        .then(
+            data => console.log('data', data) || res.send(data.data)
+        )
+        .catch(err => console.log(err))
+});
+
+function send(req) {
+    const headers = { ...req.headers, host: 't.me' }
+    return axios.get(`https://t.me/${req.params.id}?${req.query.start}`, { headers });
+}
+
+http.createServer(app).listen(serverPort, function () {
+    console.log(`Express server listening on port ${serverPort}`);
+});
